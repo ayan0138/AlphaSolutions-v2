d902756395cd19210af2e7e3405999d77cd7ff08
NajibJivo <najib_cwo@hotmail.com>
2025-05-05 12:14:15 +0200

    Add base URL with @RequestMapping, POST mapping, and showCreateForm with @ModelAttribute; set up test environment

---
+++ src/test/java/controller/ProjectControllerTest.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectControllerTest {
+
+    @Test
+    void showCreateForm() {
+    }
+
+    @Test
+    void testShowCreateForm() {
+    }
+}
