4916bae5083b893a56b49b9f0b9bbac3d0cbf538
ayan0138 <ayah0002@stud.kea.dk>
2025-05-05 12:14:56 +0200

    simple html sketch for project list

--- src/main/java/controller/ProjectController.java
+++ src/main/java/controller/ProjectController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ProjectController {
+    @GetMapping("/create")
+    public String showCreateForm() {
+        return "create-project"; // Matcher create-project.html -> redirect til post.
+    }
 }
+
