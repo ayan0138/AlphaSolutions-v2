db09af337287ab1709565f6dbedeb1f3d66b252b
NajibJivo <najib_cwo@hotmail.com>
2025-05-13 21:50:05 +0200

    feat: added save method with generated key support to ProjectRepository refactor: improved readability and documentation across all SQL methods

--- src/main/java/com/example/alphasolutionsv2/repository/RoleRepository.java
+++ src/main/java/com/example/alphasolutionsv2/repository/RoleRepository.java
@@ -1,4 +1,24 @@
 package com.example.alphasolutionsv2.repository;
 
+import com.example.alphasolutionsv2.model.Role;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class RoleRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public  RoleRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void saveRole(Role role) {
+        String checkSql = "SELECT COUNT(*) FROM Roles WHERE role_id = ?";
+        Integer count = jdbcTemplate.queryForObject(checkSql, Integer.class, role.getRoleId());
+
+        if (count != null && count == 0) {
+            String sql = "INSERT INTO Roles (role_id, role_name) VALUES (?, ?)";
+            jdbcTemplate.update(sql, role.getRoleId(), role.getRoleName());
+        }
+    }
 }
