3709528af63b7d2472e457cbea25df1b849cf71a
Lawand5 <lama0007@stud.kea.dk>
2025-05-15 14:03:13 +0200

    Fix af ReportControllerTest

--- src/test/java/com/example/alphasolutionsv2/controller/ReportControllerTest.java
+++ src/test/java/com/example/alphasolutionsv2/controller/ReportControllerTest.java
@@ -5,11 +5,12 @@ import com.example.alphasolutionsv2.service.ReportService;
 import com.example.alphasolutionsv2.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
@@ -32,10 +33,10 @@ class ReportControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private ReportService reportService;
 
-    @MockBean
+    @Autowired
     private UserService userService;
 
     private User testUser;
@@ -148,4 +149,17 @@ class ReportControllerTest {
         mockMvc.perform(get("/reports/project/1"))
                 .andExpect(status().is3xxRedirection());
     }
+    @TestConfiguration
+    static class MockConfig {
+        @Bean
+        public ReportService reportService() {
+            return Mockito.mock(ReportService.class);
+        }
+
+        @Bean
+        public UserService userService() {
+            return Mockito.mock(UserService.class);
+        }
+    }
+
 }
